e5aecd3c93a0c6045f62597f443fa678f0a8886e
programingjd <programingjd@gmail.com>
2024-01-28 22:26:27 +0100

    Restored deploy script.

--- deploy.mjs
+++ deploy.mjs
@@ -1,41 +1,8 @@
-import {underline,strikethrough,magenta} from 'https://deno.land/std/fmt/colors.ts';
-import {readableStreamFromReader} from 'https://deno.land/std/streams/mod.ts';
-import {toFileUrl} from 'https://deno.land/std/path/mod.ts';
+import defaultHeaders from './headers.json' assert {type:'json'};
+import defaultMimes from './mimes.json' assert {type:'json'};
+import {underline} from 'https://deno.land/std/fmt/colors.ts';
 import {compress as br} from 'https://deno.land/x/brotli/mod.ts';
-
-/** @type {Set<DomainName>} */
-const localDomains=new Set([
-  'localhost',
-  '127.0.0.1',
-  '::1'
-]);
-
-/**
- * @param {ServeOptions|ServeUnixOptions} options
- * @return {string}
- */
-const address=(options)=>{
-  if(options.path){
-    // noinspection JSUnresolvedVariable
-    return `${options.path}`;
-  }
-  let host=options.hostname||'0.0.0.0';
-  if(host==='0.0.0.0') host='localhost';
-  let port=options.port||80;
-  if(port===80) return host;
-  return `${host}:${port}`;
-};
-
-/** @type {Object<string,ValidateFunction<*>>} */
-const {validateDirectoryConfig_,validateMimeTypes_}=await(async()=>{
-  try{
-    return await import('./validator.js');
-  }catch(err){
-    console.error('Failed to load validators.');
-    console.error(err);
-    return {validateDirectoryConfig_:null,validateMimeTypes_:null};
-  }
-})();
+import {readableStreamFromReader} from 'https://deno.land/std/streams/mod.ts';
 
 /** @type {CacheValue} */
 const methodNotAllowed={
@@ -48,7 +15,7 @@ const methodNotAllowed={
 /**
  * @param {Request} request
  * @param {URL} url
- * @param {Deno.UnixAddr|Deno.NetAddr} remoteAddr
+ * @param {Deno.NetAddr} remoteAddr
  * @param {?[Endpoint<*>]} endpoints
  * @return {Promise<Response>}
  */
@@ -74,112 +41,15 @@ const handle=async(request,url,remoteAddr,endpoints)=>{
 }
 
 /**
+ * @param {DirectoryConfig} config
  * @param {ServeOptions} options
  * @param {string} [cwd=Deno.cwd()]
  * @return {Promise<()=>Promise<void>>}
  */
-const listen=async(options, cwd=Deno.cwd())=>{
+const listen=async(config,options, cwd=Deno.cwd())=>{
   const {signal=null}=options;
-
-  let defaultHeaders=(await import('./headers.json',{assert:{type:'json'}})).default;
-  for(const [,value] of Object.entries(defaultHeaders)){
-    if(typeof value!=='string'){
-      throw new Error('Header value for default header "`${key}`" should be a string.');
-    }
-  }
-
   /** @type {State} */
   let state=new Map();
-
-  /** @type {ValidateFunction<MimeTypes>} */
-  const validateMimeTypes=await(async()=>{
-    return (data)=>{
-      if(validateMimeTypes_&&!validateMimeTypes_(data)){
-        throw new Error(validateMimeTypes_.errors?.map(it=>`Error validating ${it.keyword}:\n${it.message}`).join('\n'));
-      }
-      return data;
-    };
-  })();
-  let defaultMimes=validateMimeTypes((await import('./mimes.json',{assert:{type:'json'}})).default);
-
-  /** @type {ValidateFunction<DirectoryConfig>} */
-  const validateDirectoryConfig=await(async()=>{
-    return (data)=>{
-      if(validateDirectoryConfig_&&!validateDirectoryConfig_(data)){
-        throw new Error(validateDirectoryConfig_.errors?.map(it=>`Error validating ${it.keyword}:\n${it.message}`).join('\n'));
-      }
-      return data;
-    }
-  })();
-
-  /** @type {Endpoint<boolean>} */
-  const updateAllEndpoint={
-    updating: false,
-    name: '/update',
-    accept: async function(request, url){
-      if(request.method!=='GET') return null;
-      if(url.pathname!=='/update') return null;
-      // noinspection JSPotentiallyInvalidUsageOfThis
-      const value=this.updating;
-      // noinspection JSPotentiallyInvalidUsageOfThis
-      this.updating=true;
-      return value;
-    },
-    handle: async function(updating){
-      if(updating) return new Response({status:429});
-      try{
-        state=await updateState();
-        return new Response(null,{status:200});
-      }finally{
-        // noinspection JSPotentiallyInvalidUsageOfThis
-        this.updating=false;
-      }
-    }
-  };
-
-  /** @type {Endpoint<{name:DirectoryName,updating:boolean}>} */
-  const updateDirectoryEndpoint={
-    updating: false,
-    name: '/update/{directory}',
-    accept: async function(request, url){
-      if(request.method!=='GET') return null;
-      const match=/^[/]update[/]([^/]+)$/.exec(url.pathname);
-      if(!match) return null;
-      const name=match[1];
-      if(name.charAt(0)==='.') return null;
-      if(localDomains.has(name)) return null;
-      // noinspection JSPotentiallyInvalidUsageOfThis
-      const updating=this.updating;
-      // noinspection JSPotentiallyInvalidUsageOfThis
-      this.updating=true;
-      return {name,updating};
-    },
-    handle: async function(accepted){
-      const {name,updating}=accepted;
-      if(updating) return new Response(null,{status:429});
-      try{
-        const newConfig=await updateDirectoryState(name);
-        if(!newConfig) throw new Error(`Could not load config for directory "${name}".`);
-        const newState=new Map();
-        for(const [domain,config] of state){
-          if(config.directory!==name){
-            newState.set(domain,config);
-          }
-        }
-        for(const domain of newConfig.domains){
-          if(newState.has(domain)) {
-            throw new Error(`Domain "${domain}" is assigned to two different directories.`);
-          }
-          newState.set(domain,newConfig);
-        }
-        state=newState;
-        return new Response(null,{status:200});
-      }finally{
-        // noinspection JSPotentiallyInvalidUsageOfThis
-        this.updating=false;
-      }
-    }
-  };
 
   /**
    * @param {string} path
@@ -225,6 +95,8 @@ const listen=async(options, cwd=Deno.cwd())=>{
     }
     return new Headers([...map.entries()]);
   }
+
+  const mtime=Date.now().toString(16);
 
   /**
    * @param {string} domain
@@ -276,7 +148,7 @@ const listen=async(options, cwd=Deno.cwd())=>{
           const filesize=stat.size
           const cacheBody=cacheThreshold===null||filesize<=cacheThreshold;
           const compress=cacheBody&&mimeEntry[1].compress;
-          const etag=`${stat.mtime.getTime().toString(16)}:${filesize.toString(16)}`;
+          const etag=`${mtime}:${filesize.toString(16)}`;
           const cacheHeaders=mergeHeaders(
             headers,
             {'content-type': mimeEntry[0],etag},
@@ -307,7 +179,7 @@ const listen=async(options, cwd=Deno.cwd())=>{
             }
           );
           console.log(`${underline(pathname||'/').padEnd(80)}   ${filesize.toString().padStart(12)}`
-                      +`   ${(body.byteLength||filesize).toString().padStart(12)}`);
+            +`   ${(body.byteLength||filesize).toString().padStart(12)}`);
         }
       }else if(it.isDirectory){
         const filename=`${path}/${name}`;
@@ -340,7 +212,7 @@ const listen=async(options, cwd=Deno.cwd())=>{
       mergedMimes,
       new Set([
         `/${dir}/directory.json`,
-      ...(config.excludes||[]).map(it=>sanitizePath(`${dir}/${it}`))
+        ...(config.excludes||[]).map(it=>sanitizePath(`${dir}/${it}`))
       ]),
       cache
     );
@@ -388,54 +260,17 @@ const listen=async(options, cwd=Deno.cwd())=>{
   }
 
   /**
-   * @param {DirectoryName} dir
-   * @param {Object<string,string>} headers
-   * @param {?[string]} modules
-   * @returns {Promise<[Endpoint<*>]>}
-   */
-  async function dynamicEndpoints(dir,headers,modules){
-    if(modules&&modules.length>0){
-      const additionalHeaders=Object.assign({...defaultHeaders},headers||{});
-      const importMod=async mod=>{
-        const url=toFileUrl(cwd);
-        url.pathname+=sanitizePath(`${dir}/${mod}`);
-        const imported=await import(url);
-        let endpoints=imported.default;
-        if(typeof endpoints==='function') endpoints=await endpoints();
-        return Array.isArray(endpoints)?endpoints:[endpoints];
-      };
-      return (await Promise.all(modules.map(importMod))).
-        flat(1).
-        map(it=>{
-          if(it.name) console.log(underline(it.name));
-          return {
-            accept: it.accept,
-            handle: async(accepted)=>await it.handle(accepted,additionalHeaders)
-          };
-        });
-    }
-    return [];
-  }
-
-  /**
-   * @param {DirectoryName} dir
+   * @param {DirectoryConfig} directoryConfig
    * @return {Promise<?DirectoryEndpoints>}
    */
-  async function updateDirectoryState(dir){
+  async function updateDirectoryState(directoryConfig){
     try{
-      console.log(`${magenta('Directory')}: ${dir}`);
-      const url=toFileUrl(cwd);
-      url.pathname+=`/${dir}/directory.json`;
-      const mod=await import(url,{assert:{type:'json'}});
-      const config=validateDirectoryConfig(mod.default);
       /** @type {[Endpoint<*>]} */
       const endpoints=[
-        await staticEndpoint(dir,config.headers,config.static),
-        ...await dynamicEndpoints(dir,config.headers,config.endpoints)
+        await staticEndpoint(cwd,config.headers,config.static)
       ].filter(it=>it);
       return {
-        directory: dir,
-        domains: config.domains,
+        domains: directoryConfig.domains,
         endpoints
       }
     }catch(err){
@@ -443,59 +278,35 @@ const listen=async(options, cwd=Deno.cwd())=>{
       return null;
     }
   }
+
   /**
+   * @param {DirectoryConfig} directoryConfig
    * @return {Promise<State>}
    */
-  async function updateState(){
+  async function updateState(directoryConfig){
     /** @type {State} */
     const state=new Map();
-    /**
-     * @param {string} name
-     * @returns {Promise<boolean>}
-     */
-    const hasConfig=async(name)=>{
-      try{
-        return (await Deno.lstat(`${cwd}/${name}/directory.json`)).isFile;
-      }catch(_){
-        return false;
+
+    const config=await updateDirectoryState(directoryConfig);
+    if(!config) throw new Error('Could not load config.');
+    for(const domain of config.domains){
+      if(state.has(domain)) {
+        throw new Error(`Domain "${domain}" is assigned to two different directories.`);
       }
-    };
-    /** @type {DirectoryEndpoints} */
-    const localEndpoints={
-      directory: '.local',
-      domains: [...localDomains],
-      endpoints: [updateAllEndpoint,updateDirectoryEndpoint]
-    };
-    for(const domain of localDomains){
-      state.set(domain,localEndpoints);
-    }
-    for await(const it of Deno.readDir(cwd)){
-      if(it.isDirectory){
-        if(it.name.charAt(0)==='.') continue;
-        if(await hasConfig(it.name)){
-          const config=await updateDirectoryState(it.name);
-          if(!config) throw new Error(`Could not load config for directory "${it.name}".`);
-          for(const domain of config.domains){
-            if(state.has(domain)) {
-              throw new Error(`Domain "${domain}" is assigned to two different directories.`);
-            }
-            state.set(domain,config);
-          }
-        }else console.log(strikethrough(`Directory: ${it.name}`));
-      }
+      state.set(domain,config);
     }
     return state;
   }
 
-  state=await updateState();
+  state=await updateState(config);
   /**
    * @param {Request} request
-   * @param {Deno.UnixAddr|Deno.NetAddr} remoteAddr
-   * @returns {Promise<Response>}
+   * @param {Deno.NetAddr} remoteAddr
+   * @returns {Promise<void>}
    */
   const handleRequest=async(request,remoteAddr)=>{
     try{
-      const url=new URL(request.url);
+      const url=new URL(request.request.url);
       const hostname=url.hostname;
       return await handle(request,url,remoteAddr,state.get(hostname)?.endpoints);
     }catch(err){
@@ -503,8 +314,6 @@ const listen=async(options, cwd=Deno.cwd())=>{
     }
   };
   const server=Deno.serve(options, handleRequest);
-  // noinspection HttpUrlsUsage
-  console.log(`Listening on http://${address(options)}.`);
   signal?.addEventListener('abort',()=>{
     console.log('stopping server');
     server.shutdown();
