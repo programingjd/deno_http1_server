c34a06293991ec6e5939f8a2f0e8fba3f0663627
programingjd <programingjd@gmail.com>
2024-01-31 23:30:55 +0100

    Fixed exclude of directory.json and fixed import of directory.json.

--- mod.js
+++ mod.js
@@ -341,7 +341,7 @@ const listen=async(options, baseUrl=toFileUrl(Deno.cwd()))=>{
       mergedHeaders,
       mergedMimes,
       new Set([
-        `/${dir}/directory.json`,
+        sanitizePath(`${dir}/directory.json`),
       ...(config.excludes||[]).map(it=>sanitizePath(`${dir}/${it}`))
       ]),
       cache
@@ -428,7 +428,7 @@ const listen=async(options, baseUrl=toFileUrl(Deno.cwd()))=>{
       console.log(`${magenta('Directory')}: ${dir}`);
       const url=new URL(baseUrl);
       url.pathname+=`/${dir}/directory.json`;
-      const mod=await import(url,{assert:{type:'json'}});
+      const mod=await import(url,{with:{type:'json'}});
       const config=validateDirectoryConfig(mod.default);
       /** @type {[Endpoint<*>]} */
       const endpoints=[
